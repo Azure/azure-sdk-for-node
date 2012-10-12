fc16eff0f77d5c73353d147d5742a7073ee5f01e
Andre Rodrigues <andrerod@microsoft.com>
2012-10-12 12:33:48 +0100

    Adding more UT

--- test/services/core/servicebussettings-tests.js
+++ test/services/core/servicebussettings-tests.js
@@ -13,3 +13,74 @@
 * limitations under the License.
 */
 
+var should = require('should');
+var url = require('url');
+
+var testutil = require('../../util/util');
+var azure = testutil.libRequire('azure');
+var Constants = azure.Constants;
+var ConnectionStringKeys = Constants.ConnectionStringKeys;
+var ServiceBusSettings = azure.ServiceBusSettings;
+
+suite('storageservicesettings-tests', function () {
+  test('testCreateFromConnectionStringWithServiceBusAutomaticCase', function () {
+    // Setup
+    var expectedNamespace = 'mynamespace';
+    var expectedServiceBusEndpoint = 'https://' + expectedNamespace + '.servicebus.windows.net';
+    var expectedWrapName = 'myname';
+    var expectedWrapPassword = 'mypassword';
+    var expectedWrapEndpointUri = 'https://' + expectedNamespace + '-sb.accesscontrol.windows.net/WRAPv0.9';
+    var connectionString = 'Endpoint=' + expectedServiceBusEndpoint + ';SharedSecretIssuer=' + expectedWrapName + ';SharedSecretValue=' + expectedWrapPassword;
+
+    // Test
+    var actual = ServiceBusSettings.createFromConnectionString(connectionString);
+
+    // Assert
+    actual._namespace.should.equal(expectedNamespace);
+    actual._serviceBusEndpointUri.should.equal(expectedServiceBusEndpoint);
+    actual._wrapName.should.equal(expectedWrapName);
+    actual._wrapPassword.should.equal(expectedWrapPassword);
+    actual._wrapEndpointUri.should.equal(expectedWrapEndpointUri);
+  });
+
+  test('testCreateFromConnectionStringWithMissingServiceBusEndpointFail', function () {
+    // Setup
+    var connectionString = 'SharedSecretIssuer=name;SharedSecretValue=password';
+
+    // Test
+    (function() {
+      ServiceBusSettings.createFromConnectionString(connectionString);
+    }).should.throw('The provided connection string ' + connectionString + ' does not have complete configuration settings.');
+  });
+
+  test('testCreateFromConnectionStringWithInvalidServiceBusKeyFail', function () {
+    // Setup
+    var invalidKey = 'InvalidKey';
+    var connectionString = invalidKey + '=value;SharedSecretIssuer=name;SharedSecretValue=password';
+
+    // Test
+    (function() {
+      ServiceBusSettings.createFromConnectionString(connectionString);
+    }).should.throw('Invalid connection string setting key ' + invalidKey.toLowerCase());
+  });
+
+  test('testCreateFromConnectionStringWithCaseInvesitive', function () {
+    // Setup
+    var expectedNamespace = 'mynamespace';
+    var expectedServiceBusEndpoint = 'https://' + expectedNamespace + '.servicebus.windows.net';
+    var expectedWrapName = 'myname';
+    var expectedWrapPassword = 'mypassword';
+    var expectedWrapEndpointUri = 'https://' + expectedNamespace + '-sb.accesscontrol.windows.net/WRAPv0.9';
+    var connectionString = 'eNdPoinT=' + expectedServiceBusEndpoint + ';sHarEdsecRetiSsuer=' + expectedWrapName + ';shArEdsecrEtvAluE=' + expectedWrapPassword;
+        
+    // Test
+    var actual = ServiceBusSettings.createFromConnectionString(connectionString);
+
+    // Assert
+    actual._namespace.should.equal(expectedNamespace);
+    actual._serviceBusEndpointUri.should.equal(expectedServiceBusEndpoint);
+    actual._wrapName.should.equal(expectedWrapName);
+    actual._wrapPassword.should.equal(expectedWrapPassword);
+    actual._wrapEndpointUri.should.equal(expectedWrapEndpointUri);
+  });
+});
