521353347135eebd515b67ab9690888cc17e0d65
Andre Rodrigues <andrerod@microsoft.com>
2012-10-14 14:22:01 +0100

    Add SB Host UT.

--- test/services/serviceBus/servicebusservice-tests.js
+++ test/services/serviceBus/servicebusservice-tests.js
@@ -1619,4 +1619,22 @@ suite('servicebusservice-tests', function () {
 
     done();
   });
+
+  test('storageConnectionStringsEndpointHttpsExplicit', function (done) {
+    var topicName = testutil.generateId(topicNamesPrefix, topicNames);
+    var expectedNamespace = process.env[ServiceClient.EnvironmentVariables.AZURE_SERVICEBUS_NAMESPACE];
+    var expectedKey = process.env[ServiceClient.EnvironmentVariables.AZURE_SERVICEBUS_ACCESS_KEY];
+    var expectedHost = 'http://' + process.env[ServiceClient.EnvironmentVariables.AZURE_SERVICEBUS_NAMESPACE] + '.servicebus.windows.net';
+    var serviceBusService = azure.createServiceBusService(expectedNamespace, expectedKey, undefined, undefined, expectedHost);
+    serviceBusService.createTopic(topicName, function (err) {
+      assert.equal(err, null);
+
+      assert.equal(serviceBusService.host, process.env[ServiceClient.EnvironmentVariables.AZURE_SERVICEBUS_NAMESPACE] + '.servicebus.windows.net');
+      assert.equal(serviceBusService.authenticationProvider.issuer, 'owner');
+      assert.equal(serviceBusService.authenticationProvider.accessKey, expectedKey);
+      assert.equal(serviceBusService.authenticationProvider.acsHost, 'https://' + process.env[ServiceClient.EnvironmentVariables.AZURE_SERVICEBUS_NAMESPACE] + '-sb.accesscontrol.windows.net:443');
+
+      done();
+    });
+  });
 });
