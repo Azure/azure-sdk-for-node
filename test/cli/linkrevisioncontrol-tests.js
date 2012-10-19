cef24f9bb5d70588dd250ab357ec5f631cb1520d
Andre Rodrigues <andrerod@microsoft.com>
2012-10-19 17:59:59 +0100

    adding UT

--- test/cli/linkrevisioncontrol-tests.js
+++ test/cli/linkrevisioncontrol-tests.js
@@ -13,10 +13,11 @@
 * limitations under the License.
 */
 
-require('should');
+var should = require('should');
 var sinon = require('sinon');
-var cli = require('../cli');
-var capture = require('../util').capture;
+var cli = require('./cli');
+var capture = require('./util').capture;
+var LinkedRevisionControl = require('../../lib/cli/linkedrevisioncontrol');
 
 var sandbox;
 
@@ -34,10 +35,73 @@ suite('cli', function(){
       done();
     });
 
-    test('should list no sites', function(done) {
-      
+    test('should get remote azure uri', function(done) {
+      // Setup
+      var azureUrl = { 
+        stdout: 'azure\tgit://github.com/andrerod/mynewsite999.git (fetch)\n' +
+                'azure\tgit://github.com/andrerod/mynewsite999.git (push)\n',
+        stderr: '' 
+      };
+
+      var cli = { output: { }, progress: function() { return { end: function() {}}} };
+      var githubClient = LinkedRevisionControl.createClient(cli, 'github');
+
+      var execStub = sandbox.stub(githubClient, '_exec');
+      execStub.yields(undefined, azureUrl);
+
+      // Test
+      githubClient._getRemoteUri(function (err, url) {
+        // Assert
+        url.should.equal('git://github.com/andrerod/mynewsite999.git');
+
+        done();
+      })
+    });
+
+    test('should get remote origin uri', function(done) {
+      // Setup
+      var originUrl = { 
+        stdout: 'origin\tgit://github.com/andrerod/mynewsite999.git (fetch)\n' +
+                'origin\tgit://github.com/andrerod/mynewsite999.git (push)\n',
+        stderr: '' 
+      };
+
+      var cli = { output: { }, progress: function() { return { end: function() {}}} };
+      var githubClient = LinkedRevisionControl.createClient(cli, 'github');
+
+      var execStub = sandbox.stub(githubClient, '_exec');
+      execStub.yields(undefined, originUrl);
+
+      // Test
+      githubClient._getRemoteUri(function (err, url) {
+        // Assert
+        url.should.equal('git://github.com/andrerod/mynewsite999.git');
+
+        done();
+      })
+    });
+
+    test('should not get other remote uris', function(done) {
+      // Setup
+      var originUrl = { 
+        stdout: 'myremote\tgit://github.com/andrerod/mynewsite999.git (fetch)\n' +
+                'myremote\tgit://github.com/andrerod/mynewsite999.git (push)\n',
+        stderr: '' 
+      };
+
+      var cli = { output: { }, progress: function() { return { end: function() {}}} };
+      var githubClient = LinkedRevisionControl.createClient(cli, 'github');
+
+      var execStub = sandbox.stub(githubClient, '_exec');
+      execStub.yields(undefined, originUrl);
+
+      // Test
+      githubClient._getRemoteUri(function (err, url) {
+        // Assert
+        should.strictEqual(undefined, url);
+
+        done();
+      })
     });
   });
 });
-
-
