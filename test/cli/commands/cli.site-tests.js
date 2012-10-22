13a7945ca65897b2f1bf9b1b8102b4012348629f
Andre Rodrigues <andrerod@microsoft.com>
2012-10-22 13:23:43 +0100

    Adding UT

--- test/cli/commands/cli.site-tests.js
+++ test/cli/commands/cli.site-tests.js
@@ -13,14 +13,59 @@
 * limitations under the License.
 */
 
-require('should');
+var should = require('should');
+var url = require('url');
+var GitHubApi = require('github');
+
 var cli = require('../cli');
 var capture = require('../util').capture;
+var LinkedRevisionControlClient = require('../../../lib/cli/linkedrevisioncontrol').LinkedRevisionControlClient;
+
+var githubUsername = process.env['AZURE_GITHUB_USERNAME'];
+var githubPassword = process.env['AZURE_GITHUB_PASSWORD'];
+var githubRepositoryUri = process.env['AZURE_GITHUB_REPOSITORY'];
+var githubClient = new GitHubApi({ version: "3.0.0" });
+
+githubClient.authenticate({
+  type: "basic",
+  username: githubUsername,
+  password: githubPassword
+});
 
 suite('cli', function(){
   suite('site', function() {
+    teardown(function (done) {
+      var repositoryName;
+
+      function deleteAllHooks (hooks, callback) {
+        if (hooks.length === 0) {
+          callback();
+        } else {
+          var hook = hooks.pop();
+          hook.user = githubUsername;
+          hook.repo = repositoryName;
+
+          githubClient.repos.deleteHook(hook, function () {
+            deleteAllHooks(hooks, callback);
+          });
+        }
+      };
+
+      // Remove any existing repository hooks
+      githubClient.repos.getFromUser({ user: githubUsername }, function (err, repositories) {
+        repositoryName = LinkedRevisionControlClient.getRepository(repositories, githubRepositoryUri).name;
+
+        githubClient.repos.getHooks({
+          user: githubUsername,
+          repo: repositoryName
+        }, function (err, hooks) {
+          deleteAllHooks(hooks, done);
+        });
+      });
+    });
+
     test('site create', function(done) {
-      var siteName = 'cliuttestsite';
+      var siteName = 'cliuttestsite1';
 
       // Setup
       var originUrl = { 
@@ -79,7 +124,7 @@ suite('cli', function(){
     });
 
     test('site create github', function(done) {
-      var siteName = 'cliuttestsite';
+      var siteName = 'cliuttestsite2';
 
       // Setup
       var originUrl = { 
@@ -92,9 +137,11 @@ suite('cli', function(){
       var cmd = ('node cli.js site create ' + siteName + ' --github --json --location').split(' ');
       cmd.push('East US');
       cmd.push('--username');
-      cmd.push(process.env['AZURE_GITHUB_USERNAME']);
+      cmd.push(githubUsername);
       cmd.push('--pass');
-      cmd.push(process.env['AZURE_GITHUB_PASSWORD']);
+      cmd.push(githubPassword);
+      cmd.push('--repository');
+      cmd.push(githubRepositoryUri);
 
       capture(function() {
         cli.parse(cmd);
@@ -114,27 +161,44 @@ suite('cli', function(){
 
           siteExists.should.be.ok;
 
-          // Delete created site
-          cmd = ('node cli.js site delete ' + siteName + ' --json --quiet').split(' ');
-          capture(function() {
-            cli.parse(cmd);
-          }, function (result) {
-            result.exitStatus.should.equal(0);
+          // verify that the hook is in github
+          githubClient.repos.getFromUser({ user: githubUsername }, function (err, repositories) {
+            var repository = LinkedRevisionControlClient.getRepository(repositories, githubRepositoryUri);
 
-            // List sites
-            cmd = 'node cli.js site list --json'.split(' ');
-            capture(function() {
-              cli.parse(cmd);
-            }, function (result) {
-              siteList = JSON.parse(result.text);
-
-              siteExists = siteList.some(function (site) {
-                return site.Name.toLowerCase() === siteName.toLowerCase()
+            githubClient.repos.getHooks({
+              user: githubUsername,
+              repo: repository.name
+            }, function (err, hooks) {
+              var hookExists = hooks.some(function (hook) {
+                var parsedUrl = url.parse(hook.config.url);
+                return parsedUrl.hostname === (siteName + '.scm.azurewebsites.net');
               });
 
-              siteExists.should.not.be.ok;
+              hookExists.should.be.ok;
 
-              done();
+              // Delete created site
+              cmd = ('node cli.js site delete ' + siteName + ' --json --quiet').split(' ');
+              capture(function() {
+                cli.parse(cmd);
+              }, function (result) {
+                result.exitStatus.should.equal(0);
+
+                // List sites
+                cmd = 'node cli.js site list --json'.split(' ');
+                capture(function() {
+                  cli.parse(cmd);
+                }, function (result) {
+                  siteList = JSON.parse(result.text);
+
+                  siteExists = siteList.some(function (site) {
+                    return site.Name.toLowerCase() === siteName.toLowerCase()
+                  });
+
+                  siteExists.should.not.be.ok;
+
+                  done();
+                });
+              });
             });
           });
         });
