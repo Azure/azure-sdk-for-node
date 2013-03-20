144293ddf7ecaa1a763591ef64f3ba49c847c36d
Andre Rodrigues <andrerod@microsoft.com>
2013-03-20 00:37:12 +0000

    Deprecate usage of connect event for streams

--- test/serviceruntime/roleenvironment-tests.js
+++ test/serviceruntime/roleenvironment-tests.js
@@ -706,9 +706,7 @@ suite('roleenvironment-tests', function () {
       serverVersionsStream = stream;
 
       stream.setEncoding('utf8');
-      stream.on('connect', function () {
-        stream.write(versionsXml);
-      });
+      stream.write(versionsXml);
 
       stream.on('end', function () {
         stream.end();
@@ -724,12 +722,11 @@ suite('roleenvironment-tests', function () {
       serverGoalStateStream = stream;
 
       stream.setEncoding('utf8');
-      stream.on('connect', function () {
-        // Write goal state every few MS
-        serverGoalStateInterval = setInterval(function () {
-          stream.write(goalStateXml);
-        }, 10);
-      });
+
+      // Write goal state every few MS
+      serverGoalStateInterval = setInterval(function () {
+        stream.write(goalStateXml);
+      }, 10);
 
       stream.on('end', function () {
         stream.end();
@@ -845,9 +842,7 @@ suite('roleenvironment-tests', function () {
       serverVersionsStream = stream;
 
       stream.setEncoding('utf8');
-      stream.on('connect', function () {
-        stream.write(versionsXml);
-      });
+      stream.write(versionsXml);
 
       stream.on('end', function () {
         stream.end();
@@ -863,12 +858,10 @@ suite('roleenvironment-tests', function () {
       serverGoalStateStream = stream;
 
       stream.setEncoding('utf8');
-      stream.on('connect', function () {
-        // Write goal state every few MS
-        serverGoalStateInterval = setInterval(function () {
-          stream.write(goalStateXml);
-        }, 10);
-      });
+      // Write goal state every few MS
+      serverGoalStateInterval = setInterval(function () {
+        stream.write(goalStateXml);
+      }, 10);
 
       stream.on('end', function () {
         stream.end();
@@ -1137,9 +1130,7 @@ suite('roleenvironment-tests', function () {
       serverVersionsStream = stream;
 
       stream.setEncoding('utf8');
-      stream.on('connect', function () {
-        stream.write(versionsXml);
-      });
+      stream.write(versionsXml);
 
       stream.on('end', function () {
         stream.end();
@@ -1155,12 +1146,10 @@ suite('roleenvironment-tests', function () {
       serverGoalStateStream = stream;
 
       stream.setEncoding('utf8');
-      stream.on('connect', function () {
-        // Write goal state every few MS
-        serverGoalStateInterval = setInterval(function () {
-          stream.write(goalStateXml);
-        }, 10);
-      });
+      // Write goal state every few MS
+      serverGoalStateInterval = setInterval(function () {
+        stream.write(goalStateXml);
+      }, 10);
 
       stream.on('end', function () {
         stream.end();
