87bbe59de7a50503fdb115ad9cb0a539e5516686
Andre Rodrigues <andrerod@microsoft.com>
2013-03-26 12:45:01 +0000

    Deprecating access condition methods on webresource

--- test/http/webresource-tests.js
+++ test/http/webresource-tests.js
@@ -21,47 +21,5 @@ var Constants = azure.Constants;
 var HeaderConstants = Constants.HeaderConstants;
 
 suite('webresource-tests', function () {
-  test('addOptionalAccessConditionHeader', function () {
-    var ifMatch = 'ifmatch';
-    var ifModifiedSince = 'ifModifiedSince';
-    var ifNoneMatch = 'ifNoneMatch';
-    var ifUnmodifiedSince = 'ifUnmodifiedSince';
-
-    var accessConditions = {
-      'If-Match': ifMatch,
-      'If-Modified-Since': ifModifiedSince,
-      'If-None-Match': ifNoneMatch,
-      'If-Unmodified-Since': ifUnmodifiedSince
-    };
-
-    var webResource = new WebResource();
-    webResource.addOptionalAccessConditionHeader(accessConditions);
-
-    webResource.headers[HeaderConstants.IF_MATCH].should.equal(ifMatch);
-    webResource.headers[HeaderConstants.IF_MODIFIED_SINCE].should.equal(ifModifiedSince);
-    webResource.headers[HeaderConstants.IF_NONE_MATCH].should.equal(ifNoneMatch);
-    webResource.headers[HeaderConstants.IF_UNMODIFIED_SINCE].should.equal(ifUnmodifiedSince);
-  });
-
-  test('addOptionalSourceAccessConditionHeader', function () {
-    var sourceIfMatch = 'sourceIfmatch';
-    var sourceIfModifiedSince = 'sourceIfModifiedSince';
-    var sourceIfNoneMatch = 'sourceIfNoneMatch';
-    var sourceIfUnmodifiedSince = 'sourceIfUnmodifiedSince';
-
-    var accessConditions = {
-      'x-ms-source-If-Match': sourceIfMatch,
-      'x-ms-source-If-Modified-Since': sourceIfModifiedSince,
-      'x-ms-source-If-None-Match': sourceIfNoneMatch,
-      'x-ms-source-If-Unmodified-Since': sourceIfUnmodifiedSince
-    };
-
-    var webResource = new WebResource();
-    webResource.addOptionalSourceAccessConditionHeader(accessConditions);
-
-    webResource.headers[HeaderConstants.SOURCE_IF_MATCH_HEADER].should.equal(sourceIfMatch);
-    webResource.headers[HeaderConstants.SOURCE_IF_MODIFIED_SINCE_HEADER].should.equal(sourceIfModifiedSince);
-    webResource.headers[HeaderConstants.SOURCE_IF_NONE_MATCH_HEADER].should.equal(sourceIfNoneMatch);
-    webResource.headers[HeaderConstants.SOURCE_IF_UNMODIFIED_SINCE_HEADER].should.equal(sourceIfUnmodifiedSince);
-  });
+  // TODO: add tests
 });
