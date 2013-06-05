69cdcc1ee5be4ca3ddac09ae9ece024e630166fa
Andre Rodrigues <andrerod@microsoft.com>
2013-06-05 11:23:54 +0100

    fix

--- test/services/serviceBus/servicebusservice-tests.js
+++ test/services/serviceBus/servicebusservice-tests.js
@@ -50,7 +50,8 @@ var testPrefix = 'servicebusservice-tests';
 
 suite('servicebusservice-tests', function () {
   suiteSetup(function (done) {
-    serviceBusService = azure.createServiceBusService();
+    serviceBusService = azure.createServiceBusService()
+      .withFilter(new azure.ExponentialRetryPolicyFilter());
 
     suiteUtil = servicebustestutil.createServiceBusTestUtils(serviceBusService, testPrefix);
     suiteUtil.setupSuite(done);
