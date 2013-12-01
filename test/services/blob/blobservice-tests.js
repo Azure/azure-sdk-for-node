a9b70a0a4f08a9ea7ac3d9b0fc35c8fbe37083f2
IvanJoukov <ivan.joukov@gmail.com>
2013-12-01 16:10:17 -0500

    Removing test

--- test/services/blob/blobservice-tests.js
+++ test/services/blob/blobservice-tests.js
@@ -1356,33 +1356,6 @@ describe('BlobService', function () {
 
       done();
     });
-
-    it('GenerateSharedAccessSignatureForBlobWithSpaceInName', function (done) {
-      var containerName = 'images';
-      var blobName = 'pic1 copy.png';
-
-      var devStorageBlobService = azure.createBlobService(ServiceClientConstants.DEVSTORE_STORAGE_ACCOUNT, ServiceClientConstants.DEVSTORE_STORAGE_ACCESS_KEY);
-
-      var sharedAccessPolicy = {
-        AccessPolicy: {
-          Permissions: BlobConstants.SharedAccessPermissions.READ,
-          Start: new Date('October 11, 2011 11:03:40 am GMT'),
-          Expiry: new Date('October 12, 2011 11:53:40 am GMT')
-        }
-      };
-
-      var sharedAccessSignature = devStorageBlobService.generateSharedAccessSignature(containerName, blobName, sharedAccessPolicy);
-
-      assert.equal(sharedAccessSignature.queryString[QueryStringConstants.SIGNED_START], '2011-10-11T11:03:40Z');
-      assert.equal(sharedAccessSignature.queryString[QueryStringConstants.SIGNED_EXPIRY], '2011-10-12T11:53:40Z');
-      assert.equal(sharedAccessSignature.queryString[QueryStringConstants.SIGNED_RESOURCE], BlobConstants.ResourceTypes.BLOB);
-      assert.equal(sharedAccessSignature.queryString[QueryStringConstants.SIGNED_PERMISSIONS], BlobConstants.SharedAccessPermissions.READ);
-      assert.equal(sharedAccessSignature.queryString[QueryStringConstants.SIGNED_VERSION], '2012-02-12');
-      assert.equal(sharedAccessSignature.queryString[QueryStringConstants.SIGNATURE], 'rHyhgZto/4+LvdTYwsd/vuevqAfiglSuAsrm7tkeBrE=');
-
-      done();
-    });
-
   });
 
   it('responseEmits', function (done) {
