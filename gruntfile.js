0de7fee5cfbf47bbf77b38d15eb2cf1a7fdb2849
yugangw-msft <yugangw@microsoft.com>
2014-10-03 15:43:35 -0700

    bump up azure-extra version

--- gruntfile.js
+++ gruntfile.js
@@ -43,7 +43,7 @@ module.exports = function(grunt) {
       'azure-scheduler': '0.9.1-pre.12',
       'azure-storage-legacy': '0.9.12',
       'azure-mgmt-authorization': '0.9.0-pre.2',
-      'azure-extra': '0.1.1'
+      'azure-extra': '0.1.2'
     },
     hydra: {
       'Microsoft.WindowsAzure.Management.Compute.Specification.dll' : {
