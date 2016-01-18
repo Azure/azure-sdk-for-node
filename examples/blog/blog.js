29c6a8a527563ae22bd7d13ddcf90ef41861185d
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-01-18 20:39:56 +0100

    :art: Update by rewrite Blog example
    - update to Express 4.*
    - update to latest Azure module version
    - dependcy updates
    - rewrite of syntax and implementation both
    of server code and Azure related code
    - add new example of using batch processes
    - add examples of using entity generators
    - update html templates markup
    - use HTML5 markup in templates
    - fix UI problems in markup
    - and other fixes and updates
    Thanks!

--- examples/blog/blog.js
+++ examples/blog/blog.js
@@ -1,55 +1,57 @@
-// 
+//
 // Copyright (c) Microsoft and contributors.  All rights reserved.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //   http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// 
+//
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// 
+//
 
 var fs = require('fs');
-if (!fs.existsSync) {
-  fs.existsSync = require('path').existsSync;
-}
 
 var azure;
-if (fs.existsSync('./../../lib/azure.js')) {
+try {
+  fs.statsSync('./../../lib/azure.js');
   azure = require('./../../lib/azure');
-} else {
+} catch (error) {
   azure = require('azure');
 }
 
-var ServiceClient = azure.ServiceClient;
 var TableQuery = azure.TableQuery;
+var TableBatch = azure.TableBatch;
+var entityGenerator = azure.TableUtilities.entityGenerator;
 var uuid = require('node-uuid');
 
 var tableName = 'posts';
 var partition = 'part1';
 
-Blog = function () {
+var Blog = function () {
   this.tableClient = azure.createTableService('UseDevelopmentStorage=true');
 };
 
 Blog.prototype.findAll = function (callback) {
-  var tableQuery = TableQuery.select()
-    .from(tableName);
+  var tableQuery = new TableQuery()
+    .select();
 
-  this.tableClient.queryEntities(tableQuery, callback);
+  this.tableClient.queryEntities(tableName, tableQuery, null, callback);
 };
 
 Blog.prototype.findById = function (id, callback) {
-  this.tableClient.queryEntity(tableName, partition, id, callback);
+  this.tableClient.retrieveEntity(tableName, partition, id, callback);
 };
 
 Blog.prototype.destroy = function (id, callback) {
-  var entity = { PartitionKey: partition, RowKey: id };
+  var entity = {
+    PartitionKey: entityGenerator.String(partition),
+    RowKey: entityGenerator.String(id)
+  };
   this.tableClient.deleteEntity(tableName, entity, callback);
 };
 
@@ -65,23 +67,18 @@ Blog.prototype.save = function (posts, callback) {
 Blog.prototype.savePosts = function (posts, callback) {
   if (posts.length === 0) {
     callback();
-  }
-  else {
+  } else {
     var post = posts.pop();
-    post.created_at = new Date();
-
-    if (post.comments === undefined)
-      post.comments = [];
-
-    for (var j = 0; j < post.comments.length; j++) {
-      post.comments[j].created_at = new Date();
+    var now = new Date().toUTCString();
+    var newPost = {
+      PartitionKey: entityGenerator.String(partition),
+      RowKey: entityGenerator.String(uuid.v4()),
+      title: entityGenerator.String(post.title),
+      body: entityGenerator.String(post.body),
+      created_at: entityGenerator.String(now)
     }
-
-    post.PartitionKey = partition;
-    post.RowKey = uuid();
-
     var provider = this;
-    this.tableClient.insertEntity(tableName, post, function () {
+    this.tableClient.insertEntity(tableName, newPost, function (error) {
       // Insert remaining posts recursively
       provider.savePosts(posts, callback);
     });
@@ -93,18 +90,42 @@ Blog.prototype.init = function () {
   this.tableClient.createTableIfNotExists(tableName, function (err, created) {
     if (created) {
       console.log('Setting up demo data ...');
-
-      provider.tableClient.beginBatch();
-
+      var batch = new TableBatch();
       var now = new Date().toUTCString();
-      provider.tableClient.insertEntity(tableName, { PartitionKey: partition, RowKey: uuid(), title: 'Post one', body: 'Body one', created_at: now });
-      provider.tableClient.insertEntity(tableName, { PartitionKey: partition, RowKey: uuid(), title: 'Post two', body: 'Body two', created_at: now });
-      provider.tableClient.insertEntity(tableName, { PartitionKey: partition, RowKey: uuid(), title: 'Post three', body: 'Body three', created_at: now });
-      provider.tableClient.insertEntity(tableName, { PartitionKey: partition, RowKey: uuid(), title: 'Post four', body: 'Body four', created_at: now });
-
-      provider.tableClient.commitBatch(function () {
-        console.log('Done');
+      batch.insertEntity({
+        PartitionKey: entityGenerator.String(partition),
+        RowKey: entityGenerator.String(uuid.v4()),
+        title: entityGenerator.String('Post one'),
+        body: entityGenerator.String('Body one'),
+        created_at: entityGenerator.String(now)
       });
+      batch.insertEntity({
+        PartitionKey: entityGenerator.String(partition),
+        RowKey: entityGenerator.String(uuid.v4()),
+        title: entityGenerator.String('Post two'),
+        body: entityGenerator.String('Body two'),
+        created_at: entityGenerator.String(now)
+      });
+      batch.insertEntity({
+        PartitionKey: entityGenerator.String(partition),
+        RowKey: entityGenerator.String(uuid.v4()),
+        title: entityGenerator.String('Post three'),
+        body: entityGenerator.String('Body three'),
+        created_at: entityGenerator.String(now)
+      });
+      batch.insertEntity({
+        PartitionKey: entityGenerator.String(partition),
+        RowKey: entityGenerator.String(uuid.v4()),
+        title: entityGenerator.String('Post four'),
+        body: entityGenerator.String('Body four'),
+        created_at: entityGenerator.String(now)
+      });
+
+      provider.tableClient.executeBatch(tableName,
+        batch,
+        function (error, insertEntities, response) {
+          console.log('Done');
+        });
     }
   });
 };
--- examples/blog/server.js
+++ examples/blog/server.js
@@ -1,46 +1,55 @@
-// 
+//
 // Copyright (c) Microsoft and contributors.  All rights reserved.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //   http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// 
+//
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// 
+//
 
 // Based on:
 // http://howtonode.org/express-mongodb
 
 var express = require('express');
+var bodyParser = require('body-parser');
+var errorHandler = require('errorhandler');
+var methodOverride = require('method-override');
+var stylus = require('stylus');
+var expressEjsLayouts = require('express-ejs-layouts');
+var path = require('path');
+
 var Blog = require('./blog').Blog;
 
-var app = module.exports = express.createServer();
+var app = module.exports = express();
 
 // Configuration
 
-app.configure(function () {
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(expressEjsLayouts);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride());
+app.use(stylus.middleware({
+  src: path.join(__dirname, 'public')
+}));
+app.use(express.static(path.join(__dirname, '/public')));
 
-app.configure('development', function () {
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
+if ('development' === app.get('env')) {
+  app.use(errorHandler());
+}
 
-app.configure('production', function () {
-  app.use(express.errorHandler());
-});
+if ('production' === app.get('env')) {
+  app.use(errorHandler());
+}
 
 // Routes
 
@@ -49,27 +58,24 @@ blog.init();
 
 app.get('/', function (req, res) {
   blog.findAll(function (error, posts) {
-    res.render('index.ejs', { locals: {
+    res.render('index.ejs', {
       title: 'Blog',
-      posts: posts
-    }
+      posts: posts.entries
     });
   });
 });
 
 app.get('/blog/new', function (req, res) {
-  res.render('blog_new.ejs', { locals: {
+  res.render('blog_new.ejs', {
     title: 'New Blog Post'
-  }
   });
 });
 
 app.post('/blog/new', function (req, res) {
   blog.save({
-    title: req.param('title'),
-    body: req.param('body')
-  },
-  function () {
+    title: req.body.title,
+    body: req.body.body
+  }, function () {
     res.redirect('/');
   });
 });
@@ -83,13 +89,14 @@ app.get('/blog/delete/:id', function (req, res) {
 app.get('/blog/:id', function (req, res) {
   blog.findById(req.params.id, function (error, post) {
     res.render('blog_show.ejs', {
-      locals: {
-        title: post.title,
-        post: post
-      }
+      title: post.title._,
+      post: post
     });
   });
 });
 
-app.listen(process.env.port || 1337);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+app.listen(app.get('port'), function () {
+  console.log(`Express server listening on port %d in %s mode`,
+    app.get('port'),
+    app.get('env'));
+});
