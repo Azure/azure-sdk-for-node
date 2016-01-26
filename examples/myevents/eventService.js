2ebd07ec62e8894744872144c80b322d3960c513
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-01-26 23:27:47 +0100

    :art: Update myevents example
    This commit updates myevents example project to Express 4.* and
    latest version of Azure Node SDK module:
    - update and bump up dependencies
    - update implementation of server to Express 4
    - update implementation of Azure SDK for Node to current version
    and current routines
    - small changes in implementation details for cleaner code and results
    handling
    - HTML markup update and rewrite to use HTML5 features, including
    form validation features
    - small code changes and fixes
    - project README update to point to original version
    
    The README has been updated as it contaisn a link to Channel 9 hosted
    video post outlining older version of this project.
    For this reason a git way to get detached version is added to readme.
    
    Thanks!

--- examples/myevents/eventService.js
+++ examples/myevents/eventService.js
@@ -1,117 +1,109 @@
-// 
+//
 // Copyright (c) Microsoft and contributors.  All rights reserved.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //   http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// 
+//
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// 
+//
 
-var azure = require('./../../lib/azure')
-  , uuid = require('node-uuid');
+var azure = require('azure'),
+  uuid = require('node-uuid');
 module.exports = EventService;
 
-function EventService (tableClient, blobClient) {
-    this.tableClient = tableClient;
-    this.blobClient = blobClient;
+function EventService(tableClient, blobClient) {
+  this.tableClient = tableClient;
+  this.blobClient = blobClient;
 };
 
 EventService.prototype = {
-	
-	showEvents: function(req, res) {
-		var self = this;
-		this.getEvents(function (resp, eventList) {
-            if (!eventList) {
-                eventList = [];
-            }			
-            self.showResults(res, eventList);
+
+  showEvents: function (req, res) {
+    var self = this;
+    this.getEvents(function (error, result, response) {
+      self.showResults(res, result.entries);
+    });
+  },
+
+  showEvent: function (req, res) {
+    var self = this;
+    this.getEvent(req.params.id, function (error, result, response) {
+      if (!result) {
+        res.render('error', {
+          title: "Error",
+          message: 'Event ' + req.params.id + ' not found.'
         });
-	},
-
-    showEvent: function(req, res) {
-        var self = this;
-        this.getEvent(req.params.id, function(resp, eventItem) {
-            if (!eventItem) {
-                res.render('error', {
-                   title: "Error",
-                   message: 'Event ' + req.params.id + ' not found.' 
-                });
-            } else {
-                res.render('detail', {
-                    title: eventItem.name,
-                    eventItem: eventItem,
-                    imageUrl: self.blobClient.getBlobUrl('photos', eventItem.RowKey),
-                });
-            }
+      } else {
+        res.render('detail', {
+          title: result.name._,
+          eventItem: result,
+          imageUrl: self.blobClient.getUrl('photos', result.RowKey._),
         });
-    },
+      }
+    });
+  },
 
-  	getEvents: function(callback) {
-  		var query = azure.TableQuery
-              .select()
-              .from('events');
+  getEvents: function (callback) {
+    var query = new azure.TableQuery();
+    this.tableClient.queryEntities('events', query, null, callback);
+  },
 
-          this.tableClient.queryEntities(query, callback);
-  	},
+  getEvent: function (id, callback) {
+    this.tableClient.retrieveEntity('events', 'myEvents', id, callback);
+  },
 
-    getEvent: function(id, callback) {
-        this.tableClient.queryEntity('events', 'myEvents', id, callback);
-    },
+  showResults: function (res, eventList) {
+    res.render('index', {
+      title: 'My Events',
+      eventList: eventList
+    });
+  },
 
-    showResults: function (res, eventList) {
-        res.render('index', { 
-            title: 'My Events', 
-            eventList: eventList 
-        });
-    },
+  newEvent: function (req, res) {
+    var self = this;
 
-    newEvent: function(req, res) {
-        var self = this;
+    var createEvent = function (error, result, response) {
+      var item = req.body.item;
+      var eg = azure.TableUtilities.entityGenerator;
+      var event = {
+        RowKey: eg.String(uuid()),
+        PartitionKey: eg.String('myEvents'),
+        date: eg.String(item.date),
+        description: eg.String(item.description),
+        name: eg.String(item.name)
+      };
 
-        var createEvent = function(resp, eventList) {
-            if (!eventList) {
-                eventList = [];
-            }
-
-            var count = eventList.length;
-
-            var item = req.body.item;
-            item.RowKey = uuid();
-            item.PartitionKey = 'myEvents';
-            
-            self.tableClient.insertEntity('events', item, function(error) {
-                if (error) {
-                   console.log(error);
-                   throw error;
-                }  
-                              
-                console.log('event created, uploading photo.');
-
-                var options = {
-                  contentType: req.files.item.file.type,
-                  metadata: { fileName: item.RowKey }
-                };
-
-                self.blobClient.createBlockBlobFromFile('photos', item.RowKey, req.files.item.file.path, options, function (error1, blockBlob, response) {
-                  if (error1) {
-                    throw error;
-                  } else {
-                    console.log(JSON.stringify(blockBlob));
-                    res.redirect('/');
-                  }
-                });
-
-               
-            });
+      self.tableClient.insertEntity('events', event, function (error, result, response) {
+        if (error) {
+          console.log(error);
+          throw error;
+        }
+        console.log('event created, uploading photo.');
+        var file = req.files[0];
+        var options = {
+          contentType: file.mimetype,
+          metadata: {
+            fileName: event.RowKey._
+          }
         };
+        self.blobClient.createBlockBlobFromLocalFile('photos', event.RowKey._, file.path, options, function (error, result, response) {
+          if (error) {
+            throw error;
+          } else {
+            console.log(JSON.stringify(result));
+            res.redirect('/');
+          }
+        });
+      });
+    };
 
-        this.getEvents(createEvent);
-    },
+    this.getEvents(createEvent);
+  },
 };
