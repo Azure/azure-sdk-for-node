2cbaf9f2d4149cf337e89b7fa572a537673d3dd4
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-01-30 22:32:25 +0100

    :art: Update ServiceBus longpolling sample
    This commit updates longpolling example
    - update some methods implementation to current
    SDK version
    - more explicit arguments in topic related methods
    - remove unused or deprecated methods
    - small fixes like white space, formatting ,etc
    
    When run this sample will output:
    > node longpolling.js
    Created the topic topic_1
    Created subscription subscription_1
    Message 0 sent
    Message received. Content:
    message0
    Message 1 sent
    Message received. Content:
    message1
    Message 2 sent
    Message received. Content:
    message2
    
    Thanks!

--- examples/samples/longpolling.js
+++ examples/samples/longpolling.js
@@ -1,32 +1,28 @@
-// 
+//
 // Copyright (c) Microsoft and contributors.  All rights reserved.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //   http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// 
+//
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// 
+//
 
 var fs = require('fs');
-if (!fs.existsSync) {
-  fs.existsSync = require('path').existsSync;
-}
-
+var path = require('path');
 var azure;
-if (fs.existsSync('./../../lib/azure.js')) {
+try {
+  fs.existsSync(path.join(__dirname, './../../lib/azure.js'));
   azure = require('./../../lib/azure');
-} else {
+} catch (error) {
   azure = require('azure');
 }
-
-var util = require('util');
 
 var topic = 'topic_1';
 var subscription = 'subscription_1';
@@ -36,17 +32,17 @@ var serviceBusClient = azure.createServiceBusService();
 var messageCount = 0;
 
 function createSubscriptionAndSend(callback) {
-  serviceBusClient.createTopic(topic, function (error) {
+  serviceBusClient.createTopicIfNotExists(topic, function (error, result, response) {
     if (error) {
       console.log(error);
     } else {
       console.log('Created the topic ' + topic);
-      serviceBusClient.createSubscription(topic, subscription, function (error2) {
-        if (error2) {
-          console.log(error2);
+      // The MatchAll filter is the default filter that is used if no filter is specified
+      serviceBusClient.createSubscription(topic, subscription, function (error, result, response) {
+        if (error) {
+          console.log(error);
         } else {
           console.log('Created subscription ' + subscription);
-
           // Send a new message every 30 seconds.
           setInterval(sendMessage, 30000);
           callback();
@@ -58,21 +54,22 @@ function createSubscriptionAndSend(callback) {
 
 function sendMessage() {
   // Send a message to the topic
-  serviceBusClient.sendTopicMessage(topic, 'message' + messageCount, function (error) {
-    console.log('Message ' + messageCount + ' sent');
+  serviceBusClient.sendTopicMessage(topic, 'message' + messageCount, function (error, result, response) {
+    console.log('Message %d sent', messageCount);
     messageCount++;
   });
 }
 
 function receiveMessages() {
-  serviceBusClient.receiveSubscriptionMessage(topic, subscription, { timeoutIntervalInS: 55 } , function (error, message) {
+  serviceBusClient.receiveSubscriptionMessage(topic, subscription, {
+    timeoutIntervalInS: 55
+  }, function (error, message, response) {
     if (error) {
       console.log(error);
     } else {
-      console.log('Message received. Content:');
-      console.log(message);
+      // Message received and deleted
+      console.log('Message received. Content: \n%s', message.body);
     }
-
     receiveMessages();
   });
 }
