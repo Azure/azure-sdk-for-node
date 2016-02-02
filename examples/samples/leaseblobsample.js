94c703cd060e622cd01d8c8c51e61d106bc9b1e6
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-02-02 22:20:22 +0100

    :art: Upate Blob lease sample
    This commit updates lease code examples to use
    latest version of Azure SDK syntax:
    - update implementation to latest version
    - use *IfNotExists* where application
    - use documented arguments in callbacks as used
    in Azure documentation website (error/result/response)
    - remove Node deprecated api and replace with update ones
    - some other small code changes like formatting, white-space, etc
    
    When run example outputs:
    node leaseblobsample.js
    Created the container leasesample
    Created the blob leasesample
    Worker acquired the lease whose ID is
    cd721937-65ec-4905-9e5b-a8ce891b9422
    Worker renewed the lease whose ID is
    cd721937-65ec-4905-9e5b-a8ce891b9422
    
    The other client outputs:
    node leaseblobsample.js
    Created the container leasesample
    Created the blob leasesample
    Worker failed in acquiring the lease.
    Worker failed in acquiring the lease.
    Worker failed in acquiring the lease.
    
    Thanks!

--- examples/samples/leaseblobsample.js
+++ examples/samples/leaseblobsample.js
@@ -1,18 +1,18 @@
-﻿// 
+﻿//
 // Copyright (c) Microsoft and contributors.  All rights reserved.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //   http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// 
+//
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// 
+//
 
 /**
 * This sample demonstrates how multiple clients can attempt to acquire a lease in order to provide a locking mechanism
@@ -30,14 +30,12 @@
 */
 
 var fs = require('fs');
-if (!fs.existsSync) {
-  fs.existsSync = require('path').existsSync;
-}
-
+var path = require('path');
 var azure;
-if (fs.existsSync('./../../lib/azure.js')) {
+try {
+  fs.statSync(path.join(__dirname, './../../lib/azure.js'));
   azure = require('./../../lib/azure');
-} else {
+} catch (error) {
   azure = require('azure');
 }
 
@@ -56,83 +54,77 @@ var blobClient = azure.createBlobService();
 
 function createContainer(callback) {
   // Step 0: Check if the target container exists.
-  blobClient.createContainerIfNotExists(container, function () {
-    console.log('Created the container ' + container);
-    callback();
+  blobClient.createContainerIfNotExists(container, function (error, result, response) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Created the container %s', container);
+      callback();
+    }
   });
 };
 
 function uploadBlob(callback) {
   // Step 1: Create text blob.
   blobClient.createBlockBlobFromText(container, blob, 'Hello world!', function () {
-    console.log('Created the blob ' + blob);
+    console.log('Created the blob %s', blob);
     callback();
   });
 };
 
 function getLease(currentLease) {
-  if (currentLease == null) {
+  if (!currentLease) {
     // Step 2a: this worker doesn't hold a lease, then try to acquire the lease.
-    blobClient.acquireLease(container, blob, function (leaseError, lease) {
-      if (lease != null) {
-        leaseId = lease.id;
-
+    blobClient.acquireLease(container, blob, function (error, result, response) {
+      if (result != null) {
+        leaseId = result.id;
         // Succeed in acquiring a lease.
-        console.log('Worker acquired the lease whose ID is ' + leaseId);
-
+        console.log('Worker acquired the lease whose ID is %s', leaseId);
         setTimeout(function () {
-          getLease(lease);
+          getLease(result);
         }, workerWithLeaseSleepTimeInMs);
-      }
-      else {
+      } else {
         // Fail in acquiring a lease.
         console.log('Worker failed in acquiring the lease.');
-
         setTimeout(function () {
-          getLease(null);
+          getLease();
         }, workerWithoutLeaseSleepTimeInMs);
       }
     });
-  }
-  else {
+  } else {
     // Step 2b: This worker holds a lease, then renew the lease.
-
-    // Traditionally there is some work this worker must do while it 
+    // Traditionally there is some work this worker must do while it
     // holds the lease on the blob prior to releasing it.
-    blobClient.renewLease(container, blob, leaseId, function (leaseError, lease) {
-      if (lease) {
-        console.log('Worker renewed the lease whose ID is ' + lease.id);
-      }
-      else {
+    blobClient.renewLease(container, blob, leaseId, function (error, result, response) {
+      if (result) {
+        console.log('Worker renewed the lease whose ID is %s', result.id);
+      } else {
         console.log('Worker failed in renewing the lease.');
       }
-
       setTimeout(function () {
-        getLease(lease);
+        getLease(result);
       }, workerWithLeaseSleepTimeInMs);
     });
   }
 };
 
-var arguments = process.argv;
+var args = process.argv;
 
-if (arguments.length > 3) {
+if (args.length > 3) {
   console.log('Incorrect number of arguments');
-}
-else if (arguments.length == 3) {
+} else if (args.length === 3) {
   // Adding a third argument on the command line, whatever it is, will delete the container before running the sample.
-  blobClient.deleteContainer(container, function () {
+  blobClient.deleteContainerIfExists(container, function (error, result, response) {
     createContainer(function () {
       uploadBlob(function () {
-        getLease(null);
+        getLease();
       });
     });
   });
-}
-else {
+} else {
   createContainer(function () {
     uploadBlob(function () {
-      getLease(null);
+      getLease();
     });
   });
 }
