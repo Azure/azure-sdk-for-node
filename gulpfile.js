c6ddf29da5799f97012630faaffee9fb7b03d7ea
huangpf <phuang@microsoft.com>
2016-03-15 23:33:59 -0700

    Use AR 0.16.0-Nightly20160315 & new spec for Compute

--- gulpfile.js
+++ gulpfile.js
@@ -100,7 +100,7 @@ var mappings = {
   }
 };
 
-var defaultAutoRestVersion = '0.16.0-Nightly20160313';
+var defaultAutoRestVersion = '0.16.0-Nightly20160315';
 var usingAutoRestVersion;
 var specRoot = args['spec-root'] || "https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master";
 var project = args['project'];
