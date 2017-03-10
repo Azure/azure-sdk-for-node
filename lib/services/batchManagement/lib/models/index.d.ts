dfbfc5551716100ca8f2ddb2616ce6d0ac02f51b
matthchr <matthchr@microsoft.com>
2017-03-10 13:25:56 -0800

    Regenerate arm batch with latest spec

--- lib/services/batchManagement/lib/models/index.d.ts
+++ lib/services/batchManagement/lib/models/index.d.ts
@@ -198,29 +198,10 @@ export interface BatchAccount extends Resource {
  * @member {string} [autoStorage.storageAccountId] The resource ID of the
  * storage account to be used for auto storage account.
  *
- * @member {string} [poolAllocationMode] The allocation mode to use for
- * creating pools in the Batch account. The pool allocation mode also affects
- * how clients may authenticate to the Batch Service API. If the mode is
- * BatchService, clients may authenticate using access keys or Azure Active
- * Directory. If the mode is UserSubscription, clients must use Azure Active
- * Directory. The default is BatchService. Possible values include:
- * 'BatchService', 'UserSubscription'
- *
- * @member {object} [keyVaultReference] A reference to the Azure key vault
- * associated with the Batch account.
- *
- * @member {string} [keyVaultReference.id] The resource ID of the Azure key
- * vault associated with the Batch account.
- *
- * @member {string} [keyVaultReference.url] The Url of the Azure key vault
- * associated with the Batch account.
- *
  */
 export interface BatchAccountUpdateParameters {
   tags?: { [propertyName: string]: string };
   autoStorage?: AutoStorageBaseProperties;
-  poolAllocationMode?: string;
-  keyVaultReference?: KeyVaultReference;
 }
 
 /**
