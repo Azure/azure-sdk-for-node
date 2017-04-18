9e6a1a01c80e94324934c1b4088b8f6e0a3d2187
Amar Zavery <amzavery@microsoft.com>
2017-04-18 08:18:22 -0700

    update gulpfile to use strict

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
+'use strict';
+
 const gulp = require('gulp');
 const args = require('yargs').argv;
 const colors = require('colors');
