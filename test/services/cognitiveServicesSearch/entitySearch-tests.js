2c69a99240e9027b7fd779d2b85cbf26498a2ddb
Tony Jin <toji@microsoft.com>
2017-11-07 20:29:16 -0800

    added ms-rest-azure to package.json, moved ms-rest-azure test earlier, adjusted indentation to 2 spaces

--- test/services/cognitiveServicesSearch/entitySearch-tests.js
+++ test/services/cognitiveServicesSearch/entitySearch-tests.js
@@ -10,24 +10,24 @@ const CognitiveServicesCredentials = require('ms-rest-azure').CognitiveServicesC
 var assert = require('assert');
 
 describe('Cognitive Services Search', function() {
-    describe('EntitySearchAPI', function() {
-        it('should return a valid response', function(done) {
-            var subscriptionKey = process.env['API_SUBSCRIPTION_KEY']
-            var credentials = new CognitiveServicesCredentials(subscriptionKey);
-            var api = new Search.EntitySearchAPI(credentials);
-            api.entitiesOperations.search('seahawks', function(err, result, request, response){
-                if (err) done(err);
-                assert.notEqual(result, null);
-                assert.notEqual(result.queryContext, null);
-                assert.equal(result.queryContext.originalQuery, 'seahawks');
-            
-                assert.notEqual(result.entities, null);
-                assert.notEqual(result.entities.value, null);
-                assert.equal(result.entities.value.length, 1);
-            
-                assert.notEqual(result.entities.value[0].contractualRules, null);
-                done();
-            });
-        });
+  describe('EntitySearchAPI', function() {
+    it('should return a valid response', function(done) {
+      var subscriptionKey = process.env['API_SUBSCRIPTION_KEY']
+      var credentials = new CognitiveServicesCredentials(subscriptionKey);
+      var api = new Search.EntitySearchAPI(credentials);
+      api.entitiesOperations.search('seahawks', function(err, result, request, response){
+        if (err) done(err);
+        assert.notEqual(result, null);
+        assert.notEqual(result.queryContext, null);
+        assert.equal(result.queryContext.originalQuery, 'seahawks');
+    
+        assert.notEqual(result.entities, null);
+        assert.notEqual(result.entities.value, null);
+        assert.equal(result.entities.value.length, 1);
+    
+        assert.notEqual(result.entities.value[0].contractualRules, null);
+        done();
+      });
     });
+  });
 });
