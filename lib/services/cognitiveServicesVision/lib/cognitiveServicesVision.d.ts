300b81250397c5b9619c93e765cce3925d861bfc
Willie Xu <wilx@microsoft.com>
2017-11-09 14:06:20 -0800

    generated vision code
    testlistarm
    
    finished vision tests
    
    vision files, tests, recordings, etc
    
    minor changes, added docs for env vars
    
    fixed tests
    
    moved done()
    
    readme version
    
    regenerated using vanilla flavor and added codegen

---
+++ lib/services/cognitiveServicesVision/lib/cognitiveServicesVision.d.ts
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
+import FaceAPIClient = require('./face/faceAPIClient');
+import * as FaceModels from './face/models';
+import ComputerVisionAPIClient = require('./computerVision/computerVisionAPIClient');
+import * as ComputerVisionModels from './computerVision/models';
+
+export { FaceAPIClient, FaceModels, ComputerVisionAPIClient, ComputerVisionModels };
