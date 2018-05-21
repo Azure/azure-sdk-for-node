4d090fd6e95717e0cfd3bdac6d435cdbd0c78542
David Desberg <david.desberg@microsoft.com>
2018-05-20 23:10:18 -0700

    update for bc with node 6

--- test/services/keyVault/keyVault-storage-tests.js
+++ test/services/keyVault/keyVault-storage-tests.js
@@ -48,6 +48,11 @@ var testStorageAccount;
 
 var _tokenFromUserCredentials = () => { 
   return new Promise ( (resolve, reject) => { 
+    if(suite.isPlayback) {
+      resolve( { 'oid': '7541419d-883d-452f-a823-56aa8bf0749f' } );
+      return;
+    }
+    
     suite.credentials.getToken( (err, token) => {
       if(err) {
         reject(err);
@@ -59,22 +64,25 @@ var _tokenFromUserCredentials = () => {
 };
     
 describe('Key Vault Storage', function () {  
-  before(async function () {
+  before(function (done) {
     suite = new SuiteBase(this, testPrefix, requiredEnvironment);
-    await new Promise( (resolve) => { suite.setupSuite(function() { resolve(); }) });
-    akvClient = new KeyVault.KeyVaultClient(new msRestAzure.KeyVaultCredentials(null, suite.credentials));
-    kvManagementClient = new KeyVaultManagementClient(suite.credentials, suite.subscriptionId);
-    storageMgmtClient = new StorageManagementClient(suite.credentials, suite.subscriptionId);
-    acclocation = process.env['AZURE_TEST_LOCATION'];
-    
-    if (!suite.isPlayback && !process.env['AZURE_PASSWORD']) {
-      throw new Error('AKV storage test recording requires user credentials');
-    }
-    
-    if (suite.isPlayback) {
-      kvManagementClient.longRunningOperationRetryTimeout = 0;
-      storageMgmtClient.longRunningOperationRetryTimeout = 0;
-    }
+    suite.setupSuite(function() {
+      akvClient = new KeyVault.KeyVaultClient(new msRestAzure.KeyVaultCredentials(null, suite.credentials));
+      kvManagementClient = new KeyVaultManagementClient(suite.credentials, suite.subscriptionId);
+      storageMgmtClient = new StorageManagementClient(suite.credentials, suite.subscriptionId);
+      acclocation = process.env['AZURE_TEST_LOCATION'];
+      
+      if (!suite.isPlayback && !process.env['AZURE_PASSWORD']) {
+        throw new Error('AKV storage test recording requires user credentials');
+      }
+      
+      if (suite.isPlayback) {
+        kvManagementClient.longRunningOperationRetryTimeout = 0;
+        storageMgmtClient.longRunningOperationRetryTimeout = 0;
+      }
+      
+      done();
+    });
   });
   
   after(function (done) {
@@ -95,100 +103,97 @@ describe('Key Vault Storage', function () {
   });
   
   // if we put below in the beforeAll hook it doesn't get successfully recorded and test playback fails
-  it('should successfully assign Key Vault the operator role on a storage account', async function() {
+  it('should successfully assign Key Vault the operator role on a storage account', function(done) {
     groupName = suite.generateId(groupPrefix, createdGroups, suite.isMocked);
     // set up sample resource group
-    if(!suite.isPlayback) {
-      await suite.createResourcegroup(groupName, acclocation);
-    }
-    
-    var testVaultName = suite.generateId(vaultPrefix, createdVaults, suite.isMocked);
-    var testStorageAccountName = suite.generateId('testakvsa', createdStorageAccounts, suite.isMocked);
+    suite.createResourcegroup(groupName, acclocation, (err, result) => {
+      var testVaultName = suite.generateId(vaultPrefix, createdVaults, suite.isMocked);
+      var testStorageAccountName = suite.generateId('testakvsa', createdStorageAccounts, suite.isMocked);
 
-    // set up test vault w/ access policy permitting user credentials' oid
-    
-    var token;
-    
-    if(!suite.isPlayback) {
-      token = await _tokenFromUserCredentials(suite.credentials);
-    } else {
-      token = { 'oid': '7541419d-883d-452f-a823-56aa8bf0749f' };
-    }
-    
-    const kvParams = 
-    {
-      location: acclocation,
-      properties: {
-          tenantId: suite.domain,
-          sku: {
-              name: 'standard'
-          },
-          accessPolicies: [
-            {
+      // set up test vault w/ access policy permitting user credentials' oid
+      _tokenFromUserCredentials().then( (token) => {
+        const kvParams = 
+        {
+          location: acclocation,
+          properties: {
               tenantId: suite.domain,
-              objectId: token.oid,
-              permissions: {
-                secrets: ['get', 'list', 'set', 'delete', 'backup', 'restore', 'recover', 'purge'],
-                storage: ['get', 'list', 'delete', 'set', 'update', 'regeneratekey', 'recover', 'purge', 'backup', 'restore', 'setsas', 'listsas', 'getsas', 'deletesas']
-              }
-            }
-          ],
-          enabledForDeployment: false,
-      }
-    };
-    
-    testVault = await kvManagementClient.vaults.createOrUpdate(groupName, testVaultName, kvParams);
-    
-    const createSaParams = {
-      location: acclocation,
-      sku: {
-          name: 'Standard_RAGRS'
-      },
-      kind: 'Storage',
-      tags: {}
-    };
-    
-    testStorageAccount = await storageMgmtClient.storageAccounts.create(groupName, testStorageAccountName, createSaParams);
-    const authorizationMgmtClient = new AuthorizationManagementClient(suite.credentials, suite.subscriptionId);
-    
-    // Find the ID for the "Storage Account Key Operator Service Role" role
-    const roleList = await authorizationMgmtClient.roleDefinitions.list('/', { 
-        'filter': "roleName eq 'Storage Account Key Operator Service Role'"
-    });
-    
-    const roleAssignmentParams = {
-        roleDefinitionId: roleList[0].id,
-        principalId: '93c27d83-f79b-4cb2-8dd4-4aa716542e74' // The Azure Key Vault Service ID
-    };
+              sku: {
+                  name: 'standard'
+              },
+              accessPolicies: [
+                {
+                  tenantId: suite.domain,
+                  objectId: token.oid,
+                  permissions: {
+                    secrets: ['get', 'list', 'set', 'delete', 'backup', 'restore', 'recover', 'purge'],
+                    storage: ['get', 'list', 'delete', 'set', 'update', 'regeneratekey', 'recover', 'purge', 'backup', 'restore', 'setsas', 'listsas', 'getsas', 'deletesas']
+                  }
+                }
+              ],
+              enabledForDeployment: false,
+          }
+        };
+        
+        const createSaParams = {
+          location: acclocation,
+          sku: {
+              name: 'Standard_RAGRS'
+          },
+          kind: 'Storage',
+          tags: {}
+        };
+        const authorizationMgmtClient = new AuthorizationManagementClient(suite.credentials, suite.subscriptionId);
+        
+        kvManagementClient.vaults.createOrUpdate(groupName, testVaultName, kvParams).then( (vault) => {
+          testVault = vault;
+          return storageMgmtClient.storageAccounts.create(groupName, testStorageAccountName, createSaParams);
+        }).then( (sa) => {
+          testStorageAccount = sa;
+          // Find the ID for the "Storage Account Key Operator Service Role" role
+          return authorizationMgmtClient.roleDefinitions.list('/', { 
+            'filter': "roleName eq 'Storage Account Key Operator Service Role'"
+          });
+        }).then( (roleList) => {
+          const roleAssignmentParams = {
+              roleDefinitionId: roleList[0].id,
+              principalId: '93c27d83-f79b-4cb2-8dd4-4aa716542e74' // The Azure Key Vault Service ID
+          };
 
-    await authorizationMgmtClient.roleAssignments.create(testStorageAccount.id, suite.generateGuid(), roleAssignmentParams);
+          return authorizationMgmtClient.roleAssignments.create(testStorageAccount.id, suite.generateGuid(), roleAssignmentParams);
+        }).then( () => { 
+          done();
+        });
+      });
+    });
   });
   
-  it('should successfully add the storage account to the vault', async function () {
-    await akvClient.setStorageAccount(testVault.properties.vaultUri, testStorageAccount.name, testStorageAccount.id, 'key1', true, {
+  it('should successfully add the storage account to the vault', function (done) {
+    akvClient.setStorageAccount(testVault.properties.vaultUri, testStorageAccount.name, testStorageAccount.id, 'key1', true, {
         regenerationPeriod : 'P30D',
         storageAccountAttributes: {
             enabled: true
         }
+    }).then( () => {
+      return akvClient.getStorageAccounts(testVault.properties.vaultUri);
+    }).then( (storageAccounts) => {
+      storageAccounts.length.should.equal(1);
+      done();
     });
-    
-    const storageAccounts = await akvClient.getStorageAccounts(testVault.properties.vaultUri);
-    storageAccounts.length.should.equal(1);
   });
   
-  it('should successfully update the active account key', async function() {
-    await akvClient.updateStorageAccount(testVault.properties.vaultUri, testStorageAccount.name, { 
+  it('should successfully update the active account key', function(done) {
+    akvClient.updateStorageAccount(testVault.properties.vaultUri, testStorageAccount.name, { 
       activeKeyName: 'key2' 
-    });
+    }).then( () => { done(); } );
   });
   
-  it('should successfully disable auto key-regen', async function() {
-    await akvClient.updateStorageAccount(testVault.properties.vaultUri, testStorageAccount.name, {
+  it('should successfully disable auto key-regen', function(done) {
+    akvClient.updateStorageAccount(testVault.properties.vaultUri, testStorageAccount.name, {
       autoRegenerateKey: false
-    });
+    }).then( () => { done(); } );
   });
   
-  it('should successfully create a SAS account template', async function() {
+  it('should successfully create a SAS account template', function(done) {
     const policy = {
       AccessPolicy: {
         Services: 'bfqt', // All services: blob, file, queue, and table
@@ -201,17 +206,23 @@ describe('Key Vault Storage', function () {
     const sasTemplate = AzureStorage.generateAccountSharedAccessSignature(testStorageAccount.name, '00000000', policy); // Instead of providing the actual key, just use '00000000' as a placeholder
   
     
-    const sasDef = await akvClient.setSasDefinition(testVault.properties.vaultUri, testStorageAccount.name, 'acctall', sasTemplate, 'account', 'PT2H' /* 2 hour validity period */, {
+    akvClient.setSasDefinition(testVault.properties.vaultUri, testStorageAccount.name, 'acctall', sasTemplate, 'account', 'PT2H' /* 2 hour validity period */, {
       sasDefinitionAttributes: { enabled: true }
+    }).then( () => {
+      return akvClient.getSasDefinitions(testVault.properties.vaultUri, testStorageAccount.name);
+    }).then( (result) => {
+      result.length.should.equal(1);
+      done();
     });
-    
-    const result = await akvClient.getSasDefinitions(testVault.properties.vaultUri, testStorageAccount.name);
-    result.length.should.equal(1);
   });
   
-  it('should successfully remove the storage account from the vault', async function() {
-    await akvClient.deleteStorageAccount(testVault.properties.vaultUri, testStorageAccount.name);
-    const storageAccounts = await akvClient.getStorageAccounts(testVault.properties.vaultUri);
-    storageAccounts.length.should.equal(0);
+  it('should successfully remove the storage account from the vault', function(done) {
+    akvClient.deleteStorageAccount(testVault.properties.vaultUri, testStorageAccount.name).then( () => {
+      akvClient.getStorageAccounts(testVault.properties.vaultUri).then( (storageAccounts) => {
+        storageAccounts.length.should.equal(0);
+        done();
+      });
+      
+    });
   });
 });
