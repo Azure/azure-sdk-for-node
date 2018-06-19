1f3aaf7f8366be4ad8a8bdf01abcf05eb5705414
Ace Eldeib <alexeldeib@gmail.com>
2018-06-18 18:23:26 -0700

    initial testing

---
+++ test/services/applicationinsightsClient/applicationinsightsClient-metrics-tests.js
@@ -0,0 +1,123 @@
+//
+// Copyright (c) Microsoft and contributors.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+'use-strict';
+
+var should = require('should');
+var Testutil = require('../../util/util');
+var ApplicationInsightsDataClient = require('../../../lib/services/applicationinsightsClient/lib/applicationInsightsDataClient');
+var msrest = require('ms-rest');
+var MockedTestUtils = require('../../framework/mocked-test-utils');
+var util = require('util');
+
+describe('App Insights metrics', function () {
+  
+  var client;
+  var suiteUtil;
+
+  before(function (done) {
+    var apiKeyOptions = {
+      inHeader: {
+        'x-api-key': 'DEMO_KEY'
+      } 
+    }
+    var credentials = new msrest.ApiKeyCredentials(apiKeyOptions);
+    client = new ApplicationInsightsDataClient(credentials);
+    suiteUtil = new MockedTestUtils(client, 'applicationinsightsClient-metrics-tests');
+    suiteUtil.setupSuite(done);
+  });
+
+  after(function (done) {
+    suiteUtil.teardownSuite(done);
+  });
+
+  beforeEach(function (done) {
+    suiteUtil.setupTest(done);
+  });
+
+  afterEach(function (done) {
+    suiteUtil.baseTeardownTest(done);
+  });
+  
+  it('can get single metric successfully', function (done) {
+    let metricId = 'availabilityResults/count';
+    client.metrics.get('DEMO_APP', metricId, function(err, result) {
+      // All results at least return one empty table
+      if (err) {
+        done(err);
+      }
+      result.should.have.property('value');
+      result.value.should.have.properties(['start', 'end', 'availabilityResults/count']);
+      should.exist(result.value['availabilityResults/count'].sum);
+      result.value['availabilityResults/count'].sum.should.be.Number()
+      done();
+    })
+  });
+
+  it('can get multiple metrics successfully', function (done) {
+    // Setup
+    var params = [
+      {
+        "id": "1",
+        "parameters": {
+          "metricId": "availabilityResults/duration"
+        }
+      },
+      {
+        "id": "second",
+        "parameters": {
+          "metricId": "availabilityResults/count",
+          "timespan": "P7D12H2M",
+          "aggregation": ["sum"]
+        }
+      }
+    ]
+
+    client.metrics.getMultiple('DEMO_APP', params, function(err, result) {
+      if (err) {
+        done(err);
+      }
+      
+      // Basic properties
+      for (var i = 0; i < result.length; i++) {
+        result[i].should.have.property('status');
+        result[i].status.should.eql(200);
+        result[i].should.have.property('body');
+      }
+
+      // Should get two items back
+      result.length.should.eql(2);
+      
+      // Sanity check
+      should.exist(result[1].body.value['availabilityResults/count'].sum)
+      done();
+    })
+  });
+
+  it('can get metrics metadata successfully', function (done) {
+    client.metrics.getMetadata('DEMO_APP', function(err, result) {
+      if (err) {
+        done(err);
+      }
+
+      // Check smattering of metadata properties (how to improve this?)
+      result.should.have.properties(['metrics', 'dimensions']);
+      result.metrics.should.have.properties(['requests/count', 'users/authenticated']);
+      result.dimensions['request/source'].should.have.property('displayName');
+      done();
+    })
+  });
+});
---
+++ test/services/applicationinsightsClient/applicationinsightsClient-query-tests.js
@@ -0,0 +1,76 @@
+//
+// Copyright (c) Microsoft and contributors.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+'use-strict';
+
+var should = require('should');
+var Testutil = require('../../util/util');
+var ApplicationInsightsDataClient = require('../../../lib/services/applicationinsightsClient/lib/applicationInsightsDataClient');
+var msrest = require('ms-rest');
+var MockedTestUtils = require('../../framework/mocked-test-utils');
+var util = require('util');
+
+describe('App Insights query', function () {
+  
+  var client;
+  var suiteUtil;
+
+  before(function (done) {
+    var apiKeyOptions = {
+      inHeader: {
+        'x-api-key': 'DEMO_KEY'
+      } 
+    }
+    var credentials = new msrest.ApiKeyCredentials(apiKeyOptions);
+    client = new ApplicationInsightsDataClient(credentials);
+    suiteUtil = new MockedTestUtils(client, 'applicationinsightsClient-query-tests');
+    suiteUtil.setupSuite(done);
+  });
+
+  after(function (done) {
+    suiteUtil.teardownSuite(done);
+  });
+
+  beforeEach(function (done) {
+    suiteUtil.setupTest(done);
+  });
+
+  afterEach(function (done) {
+    suiteUtil.baseTeardownTest(done);
+  });
+  
+  it('executes successfully', function (done) {
+    var queryBody = {
+      query: 'requests | take 10',
+      timespan: 'P2D'
+    }
+    client.query.execute('DEMO_APP', queryBody, function(err, result) {
+      // All results at least return one empty table
+      if (err) {
+        done(err);
+      }
+      should.exist(result.tables);
+      (result.tables.length).should.be.aboveOrEqual(1);
+      
+      // requests here has 37 column schema
+      should.equal(result.tables[0].columns.length, 37);
+ 
+      // This call should be able to retrieve 10 rows successfully
+      should.equal(result.tables[0].rows.length, 10);
+      done();
+    })
+  });
+});
