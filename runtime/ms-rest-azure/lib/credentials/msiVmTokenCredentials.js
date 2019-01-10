068b661a17ed7ad5d4131e8ce6270c00b779ed1d
Kamil Pajdzik <kamip@microsoft.com>
2019-01-09 19:40:44 -0800

    Add support for custom MSI endpoint

--- runtime/ms-rest-azure/lib/credentials/msiVmTokenCredentials.js
+++ runtime/ms-rest-azure/lib/credentials/msiVmTokenCredentials.js
@@ -1,29 +1,28 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See License.txt in the project root for license information. 
+// Licensed under the MIT License. See License.txt in the project root for license information.
 
 // jshint ignore: start
-// Note that this file is jshint clean except for properties inside token response 
-// which are not camel cased and they are issued by the MSI service, 
+// Note that this file is jshint clean except for properties inside token response
+// which are not camel cased and they are issued by the MSI service,
 // we do not control the shape of the response object.
 
 'use strict';
 
-const msrest = require('ms-rest');
 const request = require('request');
-
 const MSITokenCredentials = require('./msiTokenCredentials');
 
 class MSIVmTokenCredentials extends MSITokenCredentials {
   constructor(options) {
     if (!options) options = {};
     super(options);
-    if (!options.port) {
-      options.port = 50342; // default port where token service runs.
-    } else if (typeof options.port.valueOf() !== 'number') {
-      throw new Error('port must be a number.');
+
+    if (!options.msiEndpoint) {
+      options.msiEndpoint = "http://169.254.169.254/metadata/identity/oauth2/token"; // Azure Instance Metadata Service identity endpoint.
+    } else if (typeof options.msiEndpoint.valueOf() !== 'string') {
+      throw new Error('msiEndpoint must be a string.');
     }
-    
-    this.port = options.port;
+
+    this.msiEndpoint = options.msiEndpoint;
   }
 
   /**
@@ -31,10 +30,10 @@ class MSIVmTokenCredentials extends MSITokenCredentials {
    * @param  {function} callback  The callback in the form (err, result)
    * @return {function} callback
    *                       {Error} [err]  The error if any
-   *                       {object} [tokenResponse] The tokenResponse (tokenType and accessToken are the two important properties). 
+   *                       {object} [tokenResponse] The tokenResponse (tokenType and accessToken are the two important properties).
    */
   getToken(callback) {
-    const postUrl = `http://localhost:${this.port}/oauth2/token`;
+    const postUrl = this.msiEndpoint;
     const reqOptions = this.prepareRequestOptions();
     request.post(postUrl, reqOptions, (err, response, body) => {
       if (err) {
@@ -57,7 +56,6 @@ class MSIVmTokenCredentials extends MSITokenCredentials {
 
   prepareRequestOptions() {
     const resource = encodeURIComponent(this.resource);
-    const forwardSlash = encodeURIComponent('/');
     let reqOptions = {
       headers: {},
       body: ''
