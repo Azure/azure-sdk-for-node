22b3e683c878281329ba0bb047c05a071636c81a
Kamil Pajdzik <kamip@microsoft.com>
2019-02-15 14:39:40 -0800

    Regenerate azure-cognitiveservices-luis-authoring package

--- lib/services/luis/authoring/lib/lUISAuthoringClient.d.ts
+++ lib/services/luis/authoring/lib/lUISAuthoringClient.d.ts
@@ -48,6 +48,7 @@ export default class LUISAuthoringClient extends ServiceClient {
   permissions: operations.Permissions;
   pattern: operations.Pattern;
   settings: operations.Settings;
+  azureAccounts: operations.AzureAccounts;
 }
 
 export { LUISAuthoringClient, models as LUISAuthoringModels };
